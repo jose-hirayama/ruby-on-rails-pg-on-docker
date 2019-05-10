afdeaa718937a71301c3d272edbf9bbcb0568331
jose-hirayama <m.hirayama@buysell-technologies.com>
2019-05-10 19:57:06 +0900

    一旦コミット

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,21 +4,36 @@ class UsersController < ApplicationController
     # ユーザ一覧
     def index
         @users = User.all
-        render json: @users
+        # render json: @users
     end
 
     # ユーザ登録ページ
     def new
- 
+        @user = User.new
     end
 
     # ユーザ登録
     def create
-
+        @user = User.new(user_params)
+        if @user.save
+            flash[:success] = "登録が完了しました"
+            redirect_to users_path
+        else
+            render 'new'
+        end
     end
 
     #　ユーザ削除
     def destroy
-
+        @user = User.find(params[:id]).destroy
+        flash[:success] = "削除が完了しました"
+        redirect_to users_path
     end
+
+    private
+
+    def user_params
+      params.require(:user).permit(:name, :mail_address)
+    end
+
 end
